2a7f5e0ec8a3763e3282b4358177b8561ac445a1
ak-28 <arunkumarvatla@gmail.com>
2026-06-29 08:41:52 +0530

    pos enc buffer

--- src/transformer_from_scratch/models/positional_embedding.py
+++ src/transformer_from_scratch/models/positional_embedding.py
@@ -4,26 +4,25 @@ import torch.nn as nn
 import torch
 
 
-class PositionalEmbedding(nn.Module):
-    def __init__(self, d_model):
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, max_seq_length = 5000):
         super().__init__()
-        
         self.d_model = d_model
+        pe = torch.zeros(max_seq_length, d_model)
+        for pos in range(max_seq_length):
+            for i in range(int(d_model/2)):
+                denominator = math.pow(10000, 2*i/self.d_model)
+                pe[pos, 2*i] = math.sin(pos / denominator)
+                pe[pos, 2*i+1] = math.cos(pos / denominator)
+        self.register_buffer("pe", pe)
         
     def forward(self, embedding):
         B, S, d = embedding.shape
-        out_tensor = torch.zeros((S,d),device=embedding.device,dtype=torch.float32)
-        for pos in range(S):
-            for i in range(int(d/2)):
-                denominator = math.pow(10000, 2*i/self.d_model)
-                out_tensor[pos, 2*i] = math.sin(pos / denominator)
-                out_tensor[pos, 2*i+1] = math.cos(pos / denominator)
-                
-        return out_tensor
+        return self.pe[:S] # type: ignore
                 
 
-pos_embd = PositionalEmbedding(512)
+pos_enc = PositionalEncoding(512)
 x = torch.randint(0,16000, (2,5,512))
-emb = pos_embd(x)
+emb = pos_enc(x)
 print(emb.shape)
 print(emb)
